6b9a8e1773f561a7fcde3e6cc6a0a83e14f950c2
Owen Twa <owentwa3@gmail.com>
2024-03-10 18:42:37 -0700

    login

---
+++ src/main/java/com/example/slatechatbox/models/Account.java
@@ -0,0 +1,41 @@
+package com.example.slatechatbox.models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+    private String username;
+    private String password;
+    
+
+    public Account() {
+    }
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getUid() {
+        return uid;
+    }
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    
+}
