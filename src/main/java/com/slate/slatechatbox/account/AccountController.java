a39beb19362d0e1b758b43463a4149f3c8572543
Jacky Van <jackyvan1234@gmail.com>
2024-03-22 19:50:46 -0700

    reformatted code

--- src/main/java/com/slate/slatechatbox/account/AccountController.java
+++ src/main/java/com/slate/slatechatbox/account/AccountController.java
@@ -16,53 +16,48 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
-
-
 @Controller
 public class AccountController {
 
-@Autowired
-private AccountRepository accountRepo;
+    @Autowired
+    private AccountRepository accountRepo;
 
+    @GetMapping("/")
+    public RedirectView process() {
+        return new RedirectView("login");
+    }
 
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletResponse request, HttpSession session) {
+        System.out.println("login page");
+        Account account = (Account) session.getAttribute("session_account");
+        if (account == null) {
+            System.out.println("null account");
+            return "account/login";
+        } else {
+            model.addAttribute("account", account);
+            return "account/home";
+        }
+    }
 
-@GetMapping("/")
-public RedirectView process() {
-    return new RedirectView("login");
-}
-
-@GetMapping("/login")
-public String getLogin(Model model, HttpServletResponse request, HttpSession session){
-    System.out.println("login page");
-    Account account = (Account) session.getAttribute("session_account");
-    if (account == null) {
-        System.out.println("null account");
-        return "account/login";
-    }
-    else {
-        model.addAttribute("account", account);
-        return "account/home";
-    }
-}
-
-@PostMapping("/login")
-public String login(@RequestParam Map<String, String> formData, Model model, HttpServletRequest request, HttpSession session) {
-    if (formData.get("username") == null || formData.get("password") == null) {
-        return "account/login";
-    }
-    System.out.println("login post request");
-    String username = formData.get("username");
-    String password = formData.get("password");
-    List<Account> accounts = accountRepo.findByUsernameAndPassword(username, password);
-    if (accounts.isEmpty()) {
-        return "account/login";
-    }
-    else {
-        Account account = accounts.get(0);
-        request.getSession().setAttribute("session_account", account);
-        model.addAttribute("account", account);
-        return "account/home";
-    }
+    @PostMapping("/login")
+    public String login(@RequestParam Map<String, String> formData, Model model, HttpServletRequest request,
+            HttpSession session) {
+        if (formData.get("username") == null || formData.get("password") == null) {
+            return "account/login";
+        }
+        System.out.println("login post request");
+        String username = formData.get("username");
+        String password = formData.get("password");
+        List<Account> accounts = accountRepo.findByUsernameAndPassword(username, password);
+        if (accounts.isEmpty()) {
+            return "account/login";
+        } else {
+            Account account = accounts.get(0);
+            request.getSession().setAttribute("session_account", account);
+            model.addAttribute("account", account);
+            return "account/home";
+        }
     }
 
     @GetMapping("/logout")
@@ -71,36 +66,36 @@ public String login(@RequestParam Map<String, String> formData, Model model, Htt
         request.getSession().invalidate();
         return "account/logout";
 
-}
+    }
 
-@GetMapping("/register")
-public String getRegister() {
-    return "account/register";
-}
-
-@PostMapping("/account/register")
-public String registerAccount(@RequestParam Map<String, String> newaccount, HttpServletResponse response) {
-    if (newaccount.get("username") == null || newaccount.get("password") == null) {
-        response.setStatus(400);
+    @GetMapping("/register")
+    public String getRegister() {
         return "account/register";
     }
-    if (accountRepo.findByUsername(newaccount.get("username")).size() > 0) {
-        response.setStatus(409);
-        return "account/register";
-    }
-    String username = newaccount.get("username");
-    String password = newaccount.get("password");
-    accountRepo.save(new Account(username,password));
-    System.out.println("new account saved");
-    response.setStatus(201);
-    return "account/login";
-}
 
-@GetMapping("/chatroom")
-public String getChatroom(Model model, HttpServletResponse request, HttpSession session) {
-    Account account = (Account) session.getAttribute("session_account");
-    model.addAttribute("account", account);
-    return "account/chatroomPage";
-}
+    @PostMapping("/account/register")
+    public String registerAccount(@RequestParam Map<String, String> newaccount, HttpServletResponse response) {
+        if (newaccount.get("username") == null || newaccount.get("password") == null) {
+            response.setStatus(400);
+            return "account/register";
+        }
+        if (accountRepo.findByUsername(newaccount.get("username")).size() > 0) {
+            response.setStatus(409);
+            return "account/register";
+        }
+        String username = newaccount.get("username");
+        String password = newaccount.get("password");
+        accountRepo.save(new Account(username, password));
+        System.out.println("new account saved");
+        response.setStatus(201);
+        return "account/login";
+    }
+
+    @GetMapping("/chatroom")
+    public String getChatroom(Model model, HttpServletResponse request, HttpSession session) {
+        Account account = (Account) session.getAttribute("session_account");
+        model.addAttribute("account", account);
+        return "account/chatroomPage";
+    }
 
 }
